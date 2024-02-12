4fdccfac03bb154aa721f26548cae2a164f51136
vasanth1066 <vasanthanuvavi@gmail.com>
2024-02-12 23:17:43 +0530

    Add Restaurant Summary

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { Fragment } from "react";
 
 import "./App.css";
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meal/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
