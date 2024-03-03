55db6684b3197fe3a382d9b2afa6bc2a7913fa1e
vasanth1066 <vasanthanuvavi@gmail.com>
2024-03-03 21:00:10 +0530

    DIY - Showing and Enabling the delete of Cart Items

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,22 +5,47 @@ import { useContext } from "react";
 
 function Cart(props) {
   const carctx = useContext(CartContext);
-  const carttitem = (
-    <ul className={classes["cart-items"]}>
-      {carctx.items.map((items) => (
-        <li>
-          Name:{items.name} Price:{items.price} Quantity:{items.quantity}{" "}
-        </li>
-      ))}
-    </ul>
-  );
+  // const carttitem = (
+  //   <ul className={classes["cart-items"]}>
+  //     {carctx.items.map((items) => (
+  //       <li>
+  //         Name:{items.name} Price:{items.price} Quantity:{items.quantity}{" "}
+  //       </li>
+  //     ))}
+  //   </ul>
+  // );
   let totalprice = 0;
   carctx.items.forEach((item) => {
     totalprice = totalprice + Number(item.price) * item.quantity;
   });
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
   return (
     <Modal onClose={props.onClose}>
-      {carttitem}
+      {
+        <ul className={classes["cart-items"]}>
+          {carctx.items.map((items) => (
+            <li>
+              {/* Price:{items.price} Quantity:{items.quantity} */}
+              <div>
+                <span>
+                  <span className={classes.price}>Name:{items.name} -</span>
+                  <span className={classes.amount}> Price:{items.price} -</span>
+                  <span className={classes.quantity}>
+                    Quantity:{items.quantity}
+                  </span>
+                </span>
+
+                <span className={classes.actions}>
+                  <button onClick={cartItemRemoveHandler}>−</button>
+                  <button onClick={cartItemAddHandler}>+</button>
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      }
       <div className={classes.total}>
         <span> Total amount</span>
         <span> {totalprice} </span>
